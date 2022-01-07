91e76f25400376dd8193f7b78aad81283519a134
divyadima25 <61356762+divyadima25@users.noreply.github.com>
2022-01-07 14:48:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "ap-south-1"
+  access_key = "AKIA4TKH4YIQTATVL7EG"
+  secret_key = "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"
+}
+
+resource "aws_ecr_repository" "my_first_ecr_repo" {
+  name = "my-first-ecr-repo"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster"
+}
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "ap-south-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "ap-south-1c"
+}
+
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "my-first-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "866123563553.dkr.ecr.ap-south-1.amazonaws.com/my-first-ecr-repo:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 1024,
+      "cpu": 512
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 1024
+  cpu                      = 512
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "test-lb-tf"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}"
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+  }
+}
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}"
+  launch_type     = "FARGATE"
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}"
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true
+    security_groups  = ["${aws_security_group.service_security_group.id}"]
+  }
+}
+
+
+resource "aws_security_group" "service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
